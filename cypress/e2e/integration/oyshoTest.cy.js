38a8c48f2387f8cdf9fc00fa418c97258f98fc9c
Cecilia Carbajal <cecilia.carbajal@sportium.com>
2023-09-10 17:13:44 +0200

    adding test cases automation cypress

---
+++ cypress/e2e/integration/oyshoTest.cy.js
@@ -0,0 +1,79 @@
+/// <reference types="Cypress" />
+
+// Definir variables de URL y título esperado
+const baseUrl = "https://www.oysho.com/es/";
+const expectedTitle =
+  "OYSHO España / Spain | Deporte y Tiempo Libre | Sitio Oficial®";
+const expectedUrl =
+  "https://www.oysho.com/es/deporte/camiseta-tirantes-perfect-adapt-copas-c1010131105p136652022.html?colorId=800";
+const expectedRegister = "https://www.oysho.com/es/signup";
+const expectedLogged = "https://www.oysho.com/es/logon";
+
+// Caso de prueba
+describe("Prueba de automatización de Oysho", () => {
+  beforeEach(() => {
+    cy.visit(baseUrl, {
+      headers: {
+        "Accept-Encoding": "gzip, deflate",
+      },
+    });
+    cy.clearCookies();
+    cy.url().should("eq", baseUrl);
+  });
+
+    it("Verificar el título de la página", () => {
+    //cy.url().should("eq", baseUrl);
+    // Verificar el título de la página solo si Cypress pudo visitarla
+    cy.title().should("eq", expectedTitle);
+    cy.get("#onetrust-accept-btn-handler").click();
+    
+
+  });
+
+  it("Verificar navegación", () => {
+    // Verificar el título de la página solo si Cypress pudo visitarla
+    //cy.url().should("eq", baseUrl);
+    cy.get("#onetrust-accept-btn-handler").click();
+    cy.get('input[placeholder="Buscar"]').click();
+    cy.get('img[alt="Camiseta tirantes perfect-adapt copas"]').click();
+    cy.url().should('eq', expectedUrl, 'La URL cambió correctamente después del clic');
+
+  });
+
+  it("Registro de Usuario", () => {
+    //cy.get('button[data-testid="user-button"]').click({ multiple: true });
+    cy.get("#onetrust-accept-btn-handler").click();
+    cy.get('button[data-testid="user-button"]').should('be.visible').click({ multiple: true });
+    cy.wait(5000);
+    
+    cy.get('[data-testid="my-account-menu-signup"]').click();
+    cy.get('[data-testid="user-button"]').click();
+    cy.get('[data-testid="my-account-menu-signup"]').click();
+    cy.url().should(
+      "eq",
+      expectedRegister,
+      "Usuario accede a Register correctamente después del clic"
+    );
+    cy.get("#email-create-account").type("yegen53347@searpen.com");
+    cy.get("#password").type("QA1password");
+    cy.get('oy-privacy-policy input[data-testid="checkbox-input"]').check({ force: true });
+    cy.get('button[data-testid="create-account"]').click();
+
+  });
+
+  it("Inicio de sesion de Usuario", () => {
+    cy.get("#onetrust-accept-btn-handler").click();
+    cy.get('button[data-testid="user-button"]').should('be.visible').click({ multiple: true });
+    cy.wait(5000);
+    cy.url().should(
+      "eq",
+      expectedLogged,
+      "Usuario accede a Register correctamente después del clic"
+    );
+    cy.wait(2500)
+    cy.get("#email-login").type("yegen53347@searpen.com");
+    cy.get("#password").type("QA1password");
+    cy.get('button[data-testid="login-button"]').click();
+
+  });
+});
---
+++ cypress/support/index.js
@@ -0,0 +1,6 @@
+/* Cypress.on("window:before:load", (win) => {
+    // Desactivar la carga de hojas de estilo externas
+    Object.defineProperty(win, "CSS", {
+      value: null,
+    });
+  }); */
